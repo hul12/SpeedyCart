574d04aa069b19e061e5d3c9e64428b200f9a075
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-03-14 10:24:31 +0100

    Initial commit

---
+++ app/src/main/java/fr/epf/min1/speedycart/MainActivity.kt
@@ -0,0 +1,11 @@
+5-package fr.epf.min1.speedycart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
